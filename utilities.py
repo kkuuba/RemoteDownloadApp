067fee13d97aa2545f21b6795e1187a41ce81a44
Meerchant <kkuuba.k1@gmail.com>
2019-11-08 17:48:00 +0100

    code_cleanup

--- utilities.py
+++ utilities.py
@@ -2,50 +2,45 @@ from tasks import task_tab
 from tasks import zadanie
 
 
-
 def check_tasks():
-
-    for i in range(len(task_tab)): # function check if all tasks all finish and retunr flag
+    for i in range(len(task_tab)):  # function check if all tasks all finish and retunr flag
 
         if not task_tab[i].finish:
-
             return False
 
     return True
 
 
 def task_parser(dane):
+    # if dane == "open_gate\n":
 
-    #if dane == "open_gate\n":
+    if dane == "status\n":  # status block
 
-    if dane == "status\n": # status block
-
-        feedback=""
+        feedback = ""
 
         for i in range(len(task_tab)):
 
-            if check_tasks(): # if all tasks done delete all objecs from tab
+            if check_tasks():  # if all tasks done delete all objecs from tab
 
                 del task_tab[i]
 
             else:
 
-                feedback=feedback + task_tab[i].show_tasks() # show every task info
+                feedback = feedback + task_tab[i].show_tasks()  # show every task info
 
-        if feedback=="":
-            return 'Currently no tasks in process\n' # give feedback
+        if feedback == "":
+            return 'Currently no tasks in process\n'  # give feedback
         else:
             return feedback
 
-
-    else: # download block
+    else:  # download block
 
         try:
 
-            linia = dane.split("**") # split data from string
-            task_tab.append(zadanie(linia[0], linia[1])) # create new task
+            linia = dane.split("**")  # split data from string
+            task_tab.append(zadanie(linia[0], linia[1]))  # create new task
             return "Task started\n"
 
         except:
 
-            return 'bad format of task\nEnter task:\n' # feedback
+            return 'bad format of task\nEnter task:\n'  # feedback
